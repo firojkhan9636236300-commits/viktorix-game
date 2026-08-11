06dbe71315ad175ab83f57280511e8abc3a7975c
firojkhan9636236300-commits <firojkhan9636236300@gmail.com>
2026-08-11 10:55:01 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app, "(default)");
+const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { db, auth };
